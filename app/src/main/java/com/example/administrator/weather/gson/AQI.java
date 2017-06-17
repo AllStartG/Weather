5df6c74e61592ebe2c4aa0e4942befc0d1ab878f
Administrator <AllStartG>
2017-06-17 23:36:56 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/administrator/weather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.administrator.weather.gson;
+
+/**
+ * Created by Administrator on 2017/6/17 0017.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
